d41a679f2b68b47706fa73eb5b9d957a9775fb13
mosheilan942 <moisi6510@gmail.com>
2023-08-15 20:05:42 +0300

    the end

--- app.js
+++ app.js
@@ -1,43 +1,68 @@
 // const express = require('express')
 
 import * as moshe from "express";
+import { v4 as uuidv4 } from "uuid";
+import bcrypt from "bcrypt";
+// const bcrypt = require('bcrypt');
+const saltRounds = 10;
+let myPlaintextPassword = "mosheilan";
+const someOtherPlaintextPassword = "not_bacon";
 
+const Bcrypt = bcrypt;
 const app = moshe.default();
-
 const port = 3000;
 
-app.use(moshe.json())
+app.use(moshe.json());
 
 app.get("/all-user", (req, res) => {
   res.send(arr);
 });
 
 app.get("/all-user/:id", (req, res) => {
-    let user = arr.map(e => e.id).indexOf(req.body.id)
-  res.send(arr[user]);
+  let user = arr.findIndex((e) => e.id === req.params.id);
+  index != -1 ? res.send(arr[user]) : null;
 });
 
 app.post("/crate-user", (req, res) => {
-    console.log(req.body);
+  let myPlaintextPassword = req.body.password;
   let obj = {
-    id: req.body.id,
+    id: uuidv4(),
     email: req.body.email,
     password: req.body.password,
   };
-  arr.push(obj);
-  res.send(arr);
+  Bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
+    obj.password = hash;
+    arr.push(obj);
+    res.send(arr);
+  });
 });
 
 app.put("/all-user/:id", (req, res) => {
-  res.send("user updeted!");
+  let user = arr.findIndex((e) => e.id === req.params.id);
+  if (user != -1) {
+    console.log(req.body.id);
+    arr[user].id = req.body.id;
+    console.log(arr[user].id);
+    arr[user].email = req.body.email;
+    arr[user].password = req.body.password;
+    res.send(arr[user]);
+  }
 });
 
-app.delete("/all-user", (req, res) => {
-    let user = arr.map(e => e.id).indexOf(req.body.id)
-    arr.splice(user, 1)
-    res.send(arr)
+app.delete("/all-user/:id", (req, res) => {
+  let index = arr.findIndex((e) => e.id === req.params.id);
+  index != -1 ? arr.splice(index, 1) : null;
+  res.send(arr);
 });
 
+app.post("/login-user", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let valid = arr.filter((e) => e.email === email)
+  bcrypt.compare(password, valid[0].password, function (err, result) {
+    result ? res.send("User is connected") : res.send("wrong credentials");
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is up and running on port:${port}`);
