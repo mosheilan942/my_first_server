1289c9f253b5e01fd30db8aa3211a26e6d88a497
mosheilan942 <moisi6510@gmail.com>
2023-08-18 12:25:03 +0300

    add function for add Prodect

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import * as jsonfileService from "./jsonfileService.js"
-import * as moshe from "express";
+import * as all_express from "express";
 import json from "jsonfile";
 import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
@@ -12,10 +12,10 @@ const saltRounds = 10;
 let myPlaintextPassword = "mosheilan";
 const someOtherPlaintextPassword = "not_bacon";
 const Bcrypt = bcrypt;
-const app = moshe.default();
+const app = all_express.default();
 const port = 3000;
 
-app.use(moshe.json());
+app.use(all_express.json());
 
 async function main() {
 
@@ -39,101 +39,29 @@ await app.post("/crate-user", checkvalid, async(req, res) => {
 });
 
 
-await app.put("/all-user/:id", async (req, res) => {
-  json.readFile("./users.json", async function (err, obj) {
-    let email = await req.body.email;
-    console.log(email);
-    let password = req.body.password;
-    if (err) console.error(err);
-    let myPlaintextPassword = password;
-    let velid_email = false;
-    let velid_password = false;
-    // const reg = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
-    // reg.test(email) ? (velid_email = true) : null;
-
-    if (velid_email) {
-      const regpass = new RegExp(
-        /^(?=[a-zA-Z]*[a-z])(?=[a-zA-Z]*[A-Z])[a-zA-Z]{8}$/
-      );
-      regpass.test(password) ? (velid_password = true) : null;
-      if (velid_password) {
-        Bcrypt.hash(myPlaintextPassword, saltRounds, function (err, hash) {
-          password = hash;
-          let index = obj.users.findIndex((e) => e.id === req.params.id);
-          if (index != -1) {
-            const arr = obj.users;
-            arr[index].email = email;
-            arr[index].password = password;
-            json.writeFile("users.json", obj, function (err) {
-              if (err) console.error(err);
-            });
-            res.send(arr);
-          }
-        });
-      }
-    }
-  });
+await app.put("/all-user/:id", checkvalid, async (req, res) => {
+  const obj = await jsonfileService.updateUser("./users.json", req.body, req.params.id);
+  res.send(obj);
 });
 
-app.delete("/all-user/:id", (req, res) => {
-  json.readFile("./users.json", function (err, obj) {
-    let arr = obj.users;
-    if (err) console.error(err);
-    let index = arr.findIndex((e) => e.id === req.params.id);
-    index != -1 ? arr.splice(index, 1) : null;
-    json.writeFile("users.json", obj, function (err) {
-      if (err) console.error(err);
-    });
-    res.send(arr);
-  });
+await app.delete("/all-user/:id", async (req, res) => {
+  const obj = await jsonfileService.deleteUserByid("./users.json", req.params.id);
+  res.send(obj);
 });
 
-app.post("/login-user", (req, res) => {
-  let email = req.body.email;
-  let password = req.body.password;
-  let valid = arr.filter((e) => e.email === email);
-  bcrypt.compare(password, valid[0].password, function (err, result) {
-  result ? res.send("User is connected") : res.send("wrong credentials");
-  });
+await app.post("/login-user", async(req, res) => {
+  const obj = await jsonfileService.logInUser("./users.json", req.body);
+  res.send(obj);
 });
 
-app.put("/all-user/addProduct/:id", (req, res) => {
-  let id = req.params.id;
-  json.readFile("./users.json", function (err, obj) {
-    console.log(obj);
-    if (err) console.error(err);
-    let arr = obj.users;
-    let valid = arr.findIndex((e) => e.id === id);
-    if (valid != -1) {
-      let title = addProduct("https://dummyjson.com/products", id);
-      arr[valid].product = title;
-      const reqOptions = {
-        method: 'post',
-        body: JSON.stringify({ id: arr[valid].id, email: arr[valid].email, password: arr[valid].password, product: arr[valid].product}),
-        headers: {
-        'Content-Type': 'application/json'
-        }
-        }
-        fetch("https://jsonplaceholder.typicode.com/users", reqOptions)
-        .then(resp => resp.json())
-        .then(json => console.log(json));
-    }
-    
-  });
+await app.put("/all-user/addProduct/:id", async (req, res) => {
+  const obj = await jsonfileService.addProductById("./users.json", req.params.id);
+  res.send(obj);
 });
 
 app.listen(port, () => {
   console.log(`Server is up and running on port:${port}`);
 });
-
-async function addProduct(url, id) {
-  let json = await fetch(url);
-  let data = await json.json();
-  let product_current = 0;
-  let valid = data.products.filter((e) => e.id === Number(id));
-  product_current = valid.title;
-  return product_current
-}
 
 
 main()
--- jsonfileService.js
+++ jsonfileService.js
@@ -4,35 +4,35 @@ import { v4 as uuidv4 } from "uuid";
 import bcrypt from "bcrypt";
 
 const saltRounds = 10;
-// let myPlaintextPassword = "mosheilan";
-// const someOtherPlaintextPassword = "not_bacon";
 const Bcrypt = bcrypt;
 const app = moshe.default();
 const port = 3000;
 
 app.use(moshe.json());
 
-
-
 export function read(peth) {
   return new Promise((resolve, reject) => {
-    let obj = json.readFileSync(peth) 
-    resolve(obj)
-    });
-};
+    let obj = json.readFileSync(peth);
+    resolve(obj);
+  });
+}
 
+export function write(file, obj) {
+  return new Promise((resolve, reject) => {
+    let newfile = json.writeFileSync(file, obj);
+    resolve(newfile);
+  });
+}
 
 export function readUserByid(peth, id) {
-  return new Promise(async(resolve, reject) => {
-    let obj = await read(peth)
+  return new Promise(async (resolve, reject) => {
+    let obj = await read(peth);
     let index = await obj.users.findIndex((e) => e.id === id);
-    index != -1 ? resolve(obj.users[index]) : null; 
-    });
-};
-
+    index != -1 ? resolve(obj.users[index]) : null;
+  });
+}
 
 export function crateNewUser(peth, body) {
-  console.log(body.email);
   return new Promise((resolve, reject) => {
     let user = {
       id: uuidv4(),
@@ -40,7 +40,7 @@ export function crateNewUser(peth, body) {
       password: body.password,
     };
     Bcrypt.hash(user.password, saltRounds, async function (err, hash) {
-      if (err) console.error((err));
+      if (err) console.error(err);
       user.password = hash;
       const obj = await read(peth);
       const arr = obj.users;
@@ -48,25 +48,85 @@ export function crateNewUser(peth, body) {
       resolve(arr);
       const obj_update = await write("./users.json", obj);
     });
-  })
+  });
 }
 
+export function updateUser(peth, body, id) {
+  return new Promise(async (resolve, reject) => {
+    let obj = await read(peth);
+    let index = await obj.users.findIndex((e) => e.id === id);
+    if (index != -1) {
+      let password = body.password;
+      Bcrypt.hash(password, saltRounds, async function (err, hash) {
+        if (err) console.error(err);
+        password = hash;
+        const obj = await read(peth);
+        const arr = obj.users;
+        arr[index].email = body.email;
+        arr[index].password = password;
+        const obj_update = await write("./users.json", obj);
+        resolve(arr);
+      });
+    }
+  });
+}
 
-export function write(file, obj) {
-  return new Promise((resolve, reject) => {
-    let newfile = json.writeFileSync(file, obj) 
-    resolve(newfile)
+export function deleteUserByid(peth, id) {
+  return new Promise(async (resolve, reject) => {
+    let obj = await read(peth);
+    let arr = obj.users;
+    let index = await arr.findIndex((e) => e.id === id);
+    index != -1 ? arr.splice(index, 1) : null;
+    const obj_update = await write("./users.json", obj);
+    resolve(arr);
+  });
+}
+
+export function logInUser(peth, body) {
+  return new Promise(async (resolve, reject) => {
+    let email = body.email;
+    let password = body.password;
+    let obj = await read(peth);
+    let arr = obj.users;
+    let valid = arr.filter((e) => e.email === email);
+    bcrypt.compare(password, valid[0].password, function (err, result) {
+      if (err) console.error(err);
+      result ? resolve("User is connected") : resolve("wrong credentials");
     });
-}
-
-const regemail = new RegExp(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/);
-const regpass = new RegExp(/^(?=[a-zA-Z]*[a-z])(?=[a-zA-Z]*[A-Z])[a-zA-Z0-9]{8}$/);
-
-export const validation_email = function (req, res, next) {
-  let valid = false
-  regemail.test(req) ? valid =  true : null
-  return valid
+  });
 }
 
 
+export async function addProductById(peth, id) {
+  return new Promise(async (resolve, reject) => {
+    let obj = await read(peth);
+    let arr = obj.users;
+    let index = await arr.findIndex((e) => e.id === id);
+    if (index != -1) {
+      let title = await addProduct("https://dummyjson.com/products", id);
+      arr[index].product = title;
+      const obj_update = await write("./users.json", obj);
+      resolve(arr);
+      const reqOptions = {
+        method: 'post',
+        body: JSON.stringify({ id: arr[index].id, email: arr[index].email, password: arr[index].password, product: arr[index].product}),
+        headers: {
+        'Content-Type': 'application/json'
+        }
+        }
+        fetch("https://jsonplaceholder.typicode.com/users", reqOptions)
+        .then(resp => resp.json())
+        .then(json => console.log(json));
+    }
+  });
+}
 
+
+async function addProduct(url, id) {
+  let json = await fetch(url);
+  let data = await json.json();
+  let product_current = 0;
+  let valid = await data.products.filter((e) => e.id === Number(id));
+  product_current = valid[0].title;
+  return product_current
+}
